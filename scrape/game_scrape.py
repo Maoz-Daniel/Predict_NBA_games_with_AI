eed5f1b43383e6efed9ea03c410565a9135141fb
Eithan1108 <89635781+Eithan1108@users.noreply.github.com>
2024-12-08 15:50:11 +0200

    Scrape the game data

---
+++ scrape/game_scrape.py
@@ -0,0 +1,50 @@
+import os
+import asyncio
+from bs4 import BeautifulSoup  # Help to parse HTML
+from playwright.async_api import async_playwright, TimeoutError as PlaywrightTimeout  # Scrape dynamic content
+import time
+import web_scrape
+
+# Constants
+SEASONS = list(range(2018, 2025))  # 2018-2024 seasons
+DATA_DIR = "data" # Data directory
+STANDINGS_DIR = os.path.join(DATA_DIR, "standings")  # Standings data
+SCORES_DIR = os.path.join(DATA_DIR, "scores")  # Scores data
+
+standings_files = os.listdir(STANDINGS_DIR)
+
+
+# Main function
+async def scrape_game(standings_file):
+    print(f"Reading file: {standings_file}")
+    with open(standings_file, 'r') as f:
+        html = f.read()
+    soup = BeautifulSoup(html)
+    links = soup.find_all("a")
+    hrefs = [l.get("href") for l in links] # Extract the href attribute of the links
+    box_scores = [l for l in hrefs if l and "boxscore" in l and ".html" in l] # Filter links containing "boxscores"
+    box_scores = [f"https://www.basketball-reference.com{l}" for l in box_scores]  # Add the base URL to the links
+    for url in box_scores:
+        save_path = os.path.join(SCORES_DIR, url.split("/")[-1])
+        if os.path.exists(save_path):
+            print(f"File already exists: {save_path}")
+            continue
+        html = await web_scrape.get_html(url, "#content")
+        if not html:
+            continue
+        with open(save_path, "w+") as f:
+            f.write(html)
+
+
+
+async def main():
+    for f in standings_files:
+        filepath = os.path.join(STANDINGS_DIR, f)
+        await scrape_game(filepath)
+
+
+        
+
+# Run the main function using asyncio
+if __name__ == "__main__":
+    asyncio.run(main())
